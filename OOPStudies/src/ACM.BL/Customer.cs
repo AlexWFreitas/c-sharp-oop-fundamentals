1bacdee74cfb98a0484e4f7bdff023064529f0dd
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-23 14:55:27 -0300

    Chapter 4 - 40%.

---
+++ OOPStudies/src/ACM.BL/Customer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        #region Fields
+
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _workAddress;
+
+        #endregion
+
+        #region Properties
+
+        public string HomeAddress { get; set; }
+
+        public string FirstName 
+        {   get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            } 
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public string WorkAddress
+        {
+            get
+            {
+                return _workAddress;
+            }
+            set
+            {
+                _workAddress = value;
+            }
+        }
+
+        #endregion 
+    }
+}
